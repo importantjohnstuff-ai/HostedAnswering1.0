cc3eecaf5d89346b99c4fc29a2790ac1b6970086
importantjohnstuff-ai <importantjohnstuff@gmail.com>
2026-03-09 02:03:53 +0800

    Supabase

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_CONFIG = {
+    url: "https://oyorurrghhybphdmhixv.supabase.co",
+    key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95b3J1cnJnaGh5YnBoZG1oaXh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5ODc4MzMsImV4cCI6MjA4ODU2MzgzM30.1eqW__OV7cJuBDiXDbPfpb4wcNczAGWcODZyy99Lrdc"
+};
